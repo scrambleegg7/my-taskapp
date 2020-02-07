8808df73777825c223b0dcb3e1ead9476121c175
donchan <scrambleegg7@gmail.com>
2020-02-07 20:16:04 +0900

    create new project pane

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
-
+import CreateProject from './components/projects/CreateProjects';
 
 class App extends Component {
 
@@ -26,6 +26,7 @@ class App extends Component {
                     <Route path="/project/:id" component={ProjectDetails} />
                     <Route path="/signin" component={SignIn} />
                     <Route path="/signup" component={SignUp} />
+                    <Route path="/create" component={CreateProject} />
                     
                 </Switch>
             </div>
--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -6,7 +6,7 @@ const SignedOutLinks= () => {
     return (
         <ul className="right">
             <li><NavLink to="/signup">Sign up</NavLink></li>
-            <li><NavLink to="/">Login</NavLink></li>
+            <li><NavLink to="/signin">Login</NavLink></li>
         </ul>
     )
 }
