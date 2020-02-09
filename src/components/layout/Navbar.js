90c323fbc639dccb6004c9d58f2a0f9c17f83861
donchan <scrambleegg7@gmail.com>
2020-02-09 21:27:46 +0900

    navlinks  changed based on login stats

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -6,25 +6,28 @@ import SignedOutLInks from './SignedOutLinks';
 
 import  { connect } from 'react-redux'; 
 
-const Navbar= () => {
+const Navbar= (props) => {
+
+    const { auth } = props;
+    console.log("Navbar firebase state props", auth )
+
+    const links = auth.uid ? <SignedInLInks /> : <SignedOutLInks />
     return (
     <nav className="nav-wrapper grey darken-3">
         <div className="container">
 
             <Link to="/" className="brand-logo">myTaskApp Link </Link>
-            
-            <SignedInLInks />
-            <SignedOutLInks />
-            
-        
+            {links}
         </div>
     </nav>
     )
 }
 
 const mapStateToProps = (state) => {
-    console.log("navbar ",state)
+    //console.log("navbar ",state)
+    return {
+        auth: state.firebase.auth
+    }
 }
-
 
 export default connect(mapStateToProps)(Navbar);
