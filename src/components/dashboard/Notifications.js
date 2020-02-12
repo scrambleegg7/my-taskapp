49f01cb013086a055486bd68cda51a44e8719ad1
donchan <scrambleegg7@gmail.com>
2020-02-12 22:13:03 +0900

    firestore functions / notifications from firestore functions

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 
-const Notifications = () => (
+const Notifications = (props) => (
 
-    <div>
-        <h1>Notifications function</h1>
+    <div className="section">
+        <div className="card z-depth-0">
+            <div className="card-content">
+                <span className="card-title">Notifications</span>
+                <ul className="notifications">
+                    <li>notification</li>
+                    <li>notification</li>
+                </ul>
+                
+            </div>
+        </div>
     </div>
 )
 
